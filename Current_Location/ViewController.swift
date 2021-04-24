3d857efbbbf2270617f1867cc524f1a926f8a0bd
Naveen Kumar <naveen@bcoder.in>
2021-04-24 12:20:33 +0530

    To get user current Location...

--- Current_Location/ViewController.swift
+++ Current_Location/ViewController.swift
@@ -6,14 +6,72 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        }
     }
-
-
+    
+    // LocationManger Stopupdating Location
+    deinit {
+        locationManager.stopUpdatingLocation()
+    }
+    
 }
 
+
+// MARK:- Delegate method of CoreLocation
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("User allowed us to access location")
+            locationManager.startUpdatingLocation()
+        case .notDetermined:
+            print("User Notdetermined us to access location")
+        case .restricted:
+            print("User restricted us to access location")
+        case .denied:
+            print("User Denied us to access location")
+            let alertController = UIAlertController(title: "TITLE", message: "Please go to Settings and turn on the permissions", preferredStyle: .alert)
+            let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                    return
+                }
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                    })
+                }
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            alertController.addAction(cancelAction)
+            alertController.addAction(settingsAction)
+            self.present(alertController, animated: true, completion: nil)
+        default:
+            break
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("Did location updates is called")
+        let lat = manager.location?.coordinate.latitude
+        let long = manager.location?.coordinate.longitude
+        print(lat!,long!)
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Did location updates is called but failed getting location \(error.localizedDescription)")
+        locationManager.stopUpdatingLocation()
+    }
+}
