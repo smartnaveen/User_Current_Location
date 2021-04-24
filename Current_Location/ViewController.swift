1e35d3cba212cae06f8980d13053cb79138becf5
Naveen Kumar <naveen@bcoder.in>
2021-04-24 10:47:15 +0530

    Initial Commit

---
+++ Current_Location/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Current_Location
+//
+//  Created by Mr. Naveen Kumar on 24/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
